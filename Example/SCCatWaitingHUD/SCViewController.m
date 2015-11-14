d923f86ea0475f0a60929b314d53c8566e04ae2c
SergioChan <yuheng9211@qq.com>
2015-11-14 19:42:25 +0800

    Initial commit

---
+++ Example/SCCatWaitingHUD/SCViewController.m
@@ -0,0 +1,29 @@
+//
+//  SCViewController.m
+//  SCCatWaitingHUD
+//
+//  Created by SergioChan on 11/14/2015.
+//  Copyright (c) 2015 SergioChan. All rights reserved.
+//
+
+#import "SCViewController.h"
+
+@interface SCViewController ()
+
+@end
+
+@implementation SCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/SCCatWaitingHUD/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SCCatWaitingHUD
+//
+//  Created by SergioChan on 11/14/2015.
+//  Copyright (c) 2015 SergioChan. All rights reserved.
+//
+
+@import UIKit;
+#import "SCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCAppDelegate class]));
+    }
+}
